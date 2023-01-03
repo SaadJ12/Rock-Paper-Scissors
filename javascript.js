69cfd5f0159b1b8f11c1f32802d6b26aa2418871
Saad <saadjamshaid000@gmail.com>
2023-01-04 00:13:26 +0500

    Update javascript.js
    Implemented code to display game result on HTML page using the innerText property and the querySelector method

--- javascript.js
+++ javascript.js
@@ -7,7 +7,10 @@ function getComputerChoice() {
   }
     
   function playRound(playerChoice) {
-    const computerChoice = getComputerChoice();
+  let result = "";
+  const computerChoice = getComputerChoice();
+  let computerScore = 0;
+  let playerScore = 0;
      if (
       (playerChoice === 'Rock' && computerChoice === 'Scissors') ||
       (playerChoice === 'ROCK' && computerChoice === 'Scissors') ||
@@ -19,7 +22,8 @@ function getComputerChoice() {
       (playerChoice === 'paper' && computerChoice === 'Rock') ||
       (playerChoice === 'PAPER' && computerChoice === 'Rock')
     ) {
-      console.log('You win!');
+      result += "You Win";
+      playerScore ++;
     } else if (
       (playerChoice === 'PAPER' && computerChoice === 'Paper') ||
       (playerChoice === 'paper' && computerChoice === 'Paper') ||
@@ -31,75 +35,84 @@ function getComputerChoice() {
       (playerChoice === 'scissors' && computerChoice === 'Scissors') ||
       (playerChoice === 'Scissors' && computerChoice === 'Scissors')
     ) {
-      console.log("It's a tie!");
+      result += "Its a Tie";
     }    
-    else {
-      console.log('You Lost');
+      else {
+      result += "You Lost";
+      computerScore++;
     }
+    if(result === "You Win") {
+      document.querySelector(".final").innerText = result;
+    }
+    else if(result === "Its a Tie") {
+      document.querySelector(".final").innerText = result;
 
+    }
+    else {
+    document.querySelector(".final").innerText = result;   
+  }
 }
-
 
 // Following is the 5 round game code to run this type playGame(); in console
 
-function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    const index = Math.floor(Math.random() * 3);
-    return choices[index];
-  }
+// function getComputerChoice() {
+//     const choices = ['Rock', 'Paper', 'Scissors'];
+//     const index = Math.floor(Math.random() * 3);
+//     return choices[index];
+//   }
   
-function playGame() {
-    let computerScore = 0;
-    let playerScore = 0;
-    let tie = 0;
-    const playerChoice = prompt('Enter your choice (Rock, Paper or Scissors)');
+// function playGame() {
+//     let computerScore = 0;
+//     let playerScore = 0;
+//     let tie = 0;
+//     const playerChoice = prompt('Enter your choice (Rock, Paper or Scissors)');
   
-    for (let i = 0; i < 5; i++) {
-      const computerChoice = getComputerChoice();
-      let result;
+//     for (let i = 0; i < 5; i++) {
+//       const computerChoice = getComputerChoice();
+//       let result;
   
-      if (
-        (playerChoice === 'Rock' && computerChoice === 'Scissors') ||
-        (playerChoice === 'ROCK' && computerChoice === 'Scissors') ||
-        (playerChoice === 'rock' && computerChoice === 'Scissors') ||
-        (playerChoice === 'SCISSORS' && computerChoice === 'Paper') ||
-        (playerChoice === 'scissors' && computerChoice === 'Paper') ||
-        (playerChoice === 'Scissors' && computerChoice === 'Paper') ||
-        (playerChoice === 'Paper' && computerChoice === 'Rock') ||
-        (playerChoice === 'paper' && computerChoice === 'Rock') ||
-        (playerChoice === 'PAPER' && computerChoice === 'Rock')
-      ) {
-        result = 'You Win';
-        playerScore++;
-      } else if (
-        (playerChoice === 'PAPER' && computerChoice === 'Paper') ||
-        (playerChoice === 'paper' && computerChoice === 'Paper') ||
-        (playerChoice === 'Paper' && computerChoice === 'Paper') ||
-        (playerChoice === 'ROCK' && computerChoice === 'Rock') ||
-        (playerChoice === 'rock' && computerChoice === 'Rock') ||
-        (playerChoice === 'Rock' && computerChoice === 'Rock') ||
-        (playerChoice === 'SCISSORS' && computerChoice === 'Scissors') ||
-        (playerChoice === 'scissors' && computerChoice === 'Scissors') ||
-        (playerChoice === 'Scissors' && computerChoice === 'Scissors')
-      ) {
-        result = "It's a tie!";
-        tie++;
-      } else {
-        result = 'You Lost';
-        computerScore++;
-      }
-      if (playerScore > computerScore) {
-        console.log("You won the game");
-      }
-      else if (computerScore > playerScore) {
-        console.log("Computer won the game");
-      }
-      else {
-        console.log("Its a tie!");
-      }
-    }
+//       if (
+//         (playerChoice === 'Rock' && computerChoice === 'Scissors') ||
+//         (playerChoice === 'ROCK' && computerChoice === 'Scissors') ||
+//         (playerChoice === 'rock' && computerChoice === 'Scissors') ||
+//         (playerChoice === 'SCISSORS' && computerChoice === 'Paper') ||
+//         (playerChoice === 'scissors' && computerChoice === 'Paper') ||
+//         (playerChoice === 'Scissors' && computerChoice === 'Paper') ||
+//         (playerChoice === 'Paper' && computerChoice === 'Rock') ||
+//         (playerChoice === 'paper' && computerChoice === 'Rock') ||
+//         (playerChoice === 'PAPER' && computerChoice === 'Rock')
+//       ) {
+//         result = 'You Win';
+//         playerScore++;
+//       } else if (
+//         (playerChoice === 'PAPER' && computerChoice === 'Paper') ||
+//         (playerChoice === 'paper' && computerChoice === 'Paper') ||
+//         (playerChoice === 'Paper' && computerChoice === 'Paper') ||
+//         (playerChoice === 'ROCK' && computerChoice === 'Rock') ||
+//         (playerChoice === 'rock' && computerChoice === 'Rock') ||
+//         (playerChoice === 'Rock' && computerChoice === 'Rock') ||
+//         (playerChoice === 'SCISSORS' && computerChoice === 'Scissors') ||
+//         (playerChoice === 'scissors' && computerChoice === 'Scissors') ||
+//         (playerChoice === 'Scissors' && computerChoice === 'Scissors')
+//       ) {
+//         result = "It's a tie!";
+//         tie++;
+//       } else {
+//         result = 'You Lost';
+//         computerScore++;
+//       }
+//       if (playerScore > computerScore) {
+//         console.log("You won the game");
+//       }
+//       else if (computerScore > playerScore) {
+//         console.log("Computer won the game");
+//       }
+//       else {
+//         console.log("Its a tie!");
+//       }
+//     }
     
-    console.log(`Player score: ${playerScore}`);
-    console.log(`Computer score: ${computerScore}`);
-    console.log(`Ties: ${tie}`);
-  }
+//     console.log(`Player score: ${playerScore}`);
+//     console.log(`Computer score: ${computerScore}`);
+//     console.log(`Ties: ${tie}`);
+//   }
