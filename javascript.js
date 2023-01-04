745e45ae4246a0a3184ed055376723ac01a7b1a3
Saad <saadjamshaid000@gmail.com>
2023-01-04 20:51:20 +0500

    Necessary comments are added

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
-// Following is the single round game code to run this type playRound('Rock, Paper or Scissors'); in console
+//Computer has to choose randomly between following given options
 function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     const index = Math.floor(Math.random() * 3);
     return choices[index];
   }
+  //Starting scores will be 0
   let scores = {
     computer: 0,
     player: 0,
@@ -61,7 +62,7 @@ function getComputerChoice() {
     }
     document.querySelector(".round").innerText = "Round: " + round;
     function reset() {
-      //reset round to 0
+      //function to reset scores and round after the game finishes
       scores.computer = 0;
       scores.player = 0;
       scores.ties = 0;
@@ -77,75 +78,8 @@ function getComputerChoice() {
       document.querySelector(".gameResults").innerText = "" + "Player Won";
       }
     }
-      if(scores.computer >= 5) {
-        reset();
-      }
-      else if(scores.player >= 5) {
-        reset();
-      }
+      //If the score of either computer or player reaches 5 it will execute reset function
+    if (scores.computer >= 5 || scores.player >= 5) {
+      reset();
     }
-
-
-// Following is the 5 round game code to run this type playGame(); in console
-
-// function getComputerChoice() {
-//     const choices = ['Rock', 'Paper', 'Scissors'];
-//     const index = Math.floor(Math.random() * 3);
-//     return choices[index];
-//   }
-  
-// function playGame() {
-//     let computerScore = 0;
-//     let playerScore = 0;
-//     let tie = 0;
-//     const playerChoice = prompt('Enter your choice (Rock, Paper or Scissors)');
-  
-//     for (let i = 0; i < 5; i++) {
-//       const computerChoice = getComputerChoice();
-//       let result;
-  
-//       if (
-//         (playerChoice === 'Rock' && computerChoice === 'Scissors') ||
-//         (playerChoice === 'ROCK' && computerChoice === 'Scissors') ||
-//         (playerChoice === 'rock' && computerChoice === 'Scissors') ||
-//         (playerChoice === 'SCISSORS' && computerChoice === 'Paper') ||
-//         (playerChoice === 'scissors' && computerChoice === 'Paper') ||
-//         (playerChoice === 'Scissors' && computerChoice === 'Paper') ||
-//         (playerChoice === 'Paper' && computerChoice === 'Rock') ||
-//         (playerChoice === 'paper' && computerChoice === 'Rock') ||
-//         (playerChoice === 'PAPER' && computerChoice === 'Rock')
-//       ) {
-//         result = 'You Win';
-//         playerScore++;
-//       } else if (
-//         (playerChoice === 'PAPER' && computerChoice === 'Paper') ||
-//         (playerChoice === 'paper' && computerChoice === 'Paper') ||
-//         (playerChoice === 'Paper' && computerChoice === 'Paper') ||
-//         (playerChoice === 'ROCK' && computerChoice === 'Rock') ||
-//         (playerChoice === 'rock' && computerChoice === 'Rock') ||
-//         (playerChoice === 'Rock' && computerChoice === 'Rock') ||
-//         (playerChoice === 'SCISSORS' && computerChoice === 'Scissors') ||
-//         (playerChoice === 'scissors' && computerChoice === 'Scissors') ||
-//         (playerChoice === 'Scissors' && computerChoice === 'Scissors')
-//       ) {
-//         result = "It's a tie!";
-//         tie++;
-//       } else {
-//         result = 'You Lost';
-//         computerScore++;
-//       }
-//       if (playerScore > computerScore) {
-//         console.log("You won the game");
-//       }
-//       else if (computerScore > playerScore) {
-//         console.log("Computer won the game");
-//       }
-//       else {
-//         console.log("Its a tie!");
-//       }
-//     }
-    
-//     console.log(`Player score: ${playerScore}`);
-//     console.log(`Computer score: ${computerScore}`);
-//     console.log(`Ties: ${tie}`);
-//   }
+  } 
