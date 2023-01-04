d942b7993e8dba80254c709cb5fe738e378d77e9
Saad <saadjamshaid000@gmail.com>
2023-01-04 19:00:39 +0500

    Added scoring and reset functionality to the Rock Paper Scissors game, so that scores are tracked and reset after 5 rounds of play
    Implemented game scoring and reset functionality for 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,16 +1,18 @@
 // Following is the single round game code to run this type playRound('Rock, Paper or Scissors'); in console
-
 function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     const index = Math.floor(Math.random() * 3);
     return choices[index];
   }
-    
+  let scores = {
+    computer: 0,
+    player: 0,
+    ties: 0
+  };
+  let round = 0; //initialize round to 0
   function playRound(playerChoice) {
   let result = "";
   const computerChoice = getComputerChoice();
-  let computerScore = 0;
-  let playerScore = 0;
      if (
       (playerChoice === 'Rock' && computerChoice === 'Scissors') ||
       (playerChoice === 'ROCK' && computerChoice === 'Scissors') ||
@@ -23,7 +25,8 @@ function getComputerChoice() {
       (playerChoice === 'PAPER' && computerChoice === 'Rock')
     ) {
       result += "You Win";
-      playerScore ++;
+      scores.player += 1;
+      round++;
     } else if (
       (playerChoice === 'PAPER' && computerChoice === 'Paper') ||
       (playerChoice === 'paper' && computerChoice === 'Paper') ||
@@ -36,22 +39,39 @@ function getComputerChoice() {
       (playerChoice === 'Scissors' && computerChoice === 'Scissors')
     ) {
       result += "Its a Tie";
+      round++;
+      scores.ties += 1;
     }    
       else {
       result += "You Lost";
-      computerScore++;
+      scores.computer += 1;
+      round++;
     }
     if(result === "You Win") {
       document.querySelector(".final").innerText = result;
+      document.querySelector(".playerScore").innerText = "PlayerScore: " + scores.player;
     }
     else if(result === "Its a Tie") {
       document.querySelector(".final").innerText = result;
-
+      document.querySelector(".ties").innerText = "Ties: " + scores.ties;
     }
     else {
-    document.querySelector(".final").innerText = result;   
+      document.querySelector(".final").innerText = result;
+      document.querySelector(".computerScore").innerText = "Computer Score: " + scores.computer;
+    }
+    document.querySelector(".round").innerText = "Round: " + round;
+    if(round > 5) {
+      //reset round to 0
+      scores.computer = 0;
+      scores.player = 0;
+      scores.ties = 0;
+      round = 0;
+      document.querySelector(".playerScore").innerText = "PlayerScore: " + 0;
+      document.querySelector(".ties").innerText = "Ties: " + 0;
+      document.querySelector(".computerScore").innerText = "Computer Score: " + 0;
+      document.querySelector(".round").innerText = "Round: " + "Game Finished"
+    }
   }
-}
 
 // Following is the 5 round game code to run this type playGame(); in console
 
